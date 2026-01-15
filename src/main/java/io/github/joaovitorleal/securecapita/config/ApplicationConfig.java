ab7ba7d49724afc207b5424c5832f43af890680c
joaoVitorLeal <joaoleal98@outlook.com>
2026-01-15 11:27:33 -0300

    feat: implement ThreadPoolTaskExecutors for general and email tasks

--- src/main/java/io/github/joaovitorleal/securecapita/config/ApplicationConfig.java
+++ src/main/java/io/github/joaovitorleal/securecapita/config/ApplicationConfig.java
@@ -1,10 +1,14 @@
 package io.github.joaovitorleal.securecapita.config;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.TimeZone;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 @EnableAsync
@@ -13,5 +17,29 @@ public class ApplicationConfig {
     @PostConstruct
     public void setDefaultTimeZone() {
         TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        int cores = Runtime.getRuntime().availableProcessors(); // Número de processadores
+        executor.setCorePoolSize(cores);
+        executor.setMaxPoolSize(cores * 2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("AppAsync-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean(name = "emailExecutor")
+    public Executor emailExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10); // Mantém 10 threads sempre ativas
+        executor.setMaxPoolSize(50); // Sobe até 50 threads caso a fila fique lotada
+        executor.setQueueCapacity(25); // Fila de espera suporta 25 emails
+        executor.setThreadNamePrefix("EmailThread-");
+        executor.initialize();
+        return executor;
     }
 }
