b60fdf7138c1795986b8581878634ad6d749edae
joaoVitorLeal <joaoleal98@outlook.com>
2026-01-15 11:28:05 -0300

    refactor: improve CustomAccessDeniedHandler

--- src/main/java/io/github/joaovitorleal/securecapita/security/handler/CustomAccessDeniedHandler.java
+++ src/main/java/io/github/joaovitorleal/securecapita/security/handler/CustomAccessDeniedHandler.java
@@ -21,19 +21,33 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
+    public static final String ACCESS_DENIED_MESSAGE = "You don't have enough permission.";
+
+    private final ObjectMapper objectMapper;
+
+    public CustomAccessDeniedHandler(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        String errorMessage = (accessDeniedException != null && accessDeniedException.getMessage() != null)
+                ? accessDeniedException.getMessage()
+                : ACCESS_DENIED_MESSAGE;
+
         ApiResponseDto httpResponse = ApiResponseDto.builder()
                 .timestamp(Instant.now().toString())
-                .reason("You don't have enough permission.")
+                .reason(errorMessage)
                 .status(HttpStatus.FORBIDDEN)
                 .statusCode(HttpStatus.FORBIDDEN.value())
                 .build();
+
         response.setContentType(APPLICATION_JSON_VALUE);
         response.setStatus(HttpStatus.FORBIDDEN.value());
+
         OutputStream out = response.getOutputStream();
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(out, httpResponse);
+        objectMapper.writeValue(out, httpResponse);
         out.flush();
     }
 }
