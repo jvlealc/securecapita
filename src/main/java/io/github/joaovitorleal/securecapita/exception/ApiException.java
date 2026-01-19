08cc883f3947c494d55b353e25e3aa2619dd38d8
joaoVitorLeal <joaoleal98@outlook.com>
2026-01-19 19:44:39 -0300

    feat: add HTTP Status response

--- src/main/java/io/github/joaovitorleal/securecapita/exception/ApiException.java
+++ src/main/java/io/github/joaovitorleal/securecapita/exception/ApiException.java
@@ -1,5 +1,9 @@
 package io.github.joaovitorleal.securecapita.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public abstract class ApiException extends RuntimeException {
 
     public ApiException(String message) {
--- src/main/java/io/github/joaovitorleal/securecapita/exception/NotificationFailureException.java
+++ src/main/java/io/github/joaovitorleal/securecapita/exception/NotificationFailureException.java
@@ -1,5 +1,9 @@
 package io.github.joaovitorleal.securecapita.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_GATEWAY)
 public abstract class NotificationFailureException extends ApiException {
 
     public NotificationFailureException(String message) {
