479c69f76d00f5cb40b4ca9f3cf38f1c0c72bd26
joaoVitorLeal <joaoleal98@outlook.com>
2026-01-19 21:11:32 -0300

    refactor: change UserNotFoundByIdException superclass to ResourceNotFoundException

--- src/main/java/io/github/joaovitorleal/securecapita/exception/UserNotFoundByIdException.java
+++ src/main/java/io/github/joaovitorleal/securecapita/exception/UserNotFoundByIdException.java
@@ -1,6 +1,6 @@
 package io.github.joaovitorleal.securecapita.exception;
 
-public class UserNotFoundByIdException extends  ApiException {
+public class UserNotFoundByIdException extends ResourceNotFoundException {
 
     public UserNotFoundByIdException(String message) {
         super(message);
