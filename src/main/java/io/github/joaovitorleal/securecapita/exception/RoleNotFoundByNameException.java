64e41d821a1779b8a0837026456cb3d2771c6e37
joaoVitorLeal <joaoleal98@outlook.com>
2026-01-19 21:12:49 -0300

    refactor: improve constructor

--- src/main/java/io/github/joaovitorleal/securecapita/exception/RoleNotFoundByNameException.java
+++ src/main/java/io/github/joaovitorleal/securecapita/exception/RoleNotFoundByNameException.java
@@ -2,7 +2,7 @@ package io.github.joaovitorleal.securecapita.exception;
 
 public class RoleNotFoundByNameException extends ApiException {
 
-    public RoleNotFoundByNameException(String message) {
-        super(message);
+    public RoleNotFoundByNameException(String roleName) {
+        super("Role not found with name: " + roleName);
     }
 }
