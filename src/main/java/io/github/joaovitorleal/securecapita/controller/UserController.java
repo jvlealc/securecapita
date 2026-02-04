8126f71f7f9708ec166f7e5720860d1907a2b042
joaoVitorLeal <joaoleal98@outlook.com>
2026-02-03 21:40:24 -0300

    feat: implement reset password for unauthenticated user feature (recovery user account)

--- src/main/java/io/github/joaovitorleal/securecapita/controller/UserController.java
+++ src/main/java/io/github/joaovitorleal/securecapita/controller/UserController.java
@@ -2,21 +2,21 @@ package io.github.joaovitorleal.securecapita.controller;
 
 import io.github.joaovitorleal.securecapita.controller.utils.UriGenerator;
 import io.github.joaovitorleal.securecapita.domain.User;
-import io.github.joaovitorleal.securecapita.dto.ApiResponseDto;
-import io.github.joaovitorleal.securecapita.dto.MfaVerificationRequestDto;
-import io.github.joaovitorleal.securecapita.dto.UserRequestDto;
-import io.github.joaovitorleal.securecapita.dto.UserResponseDto;
+import io.github.joaovitorleal.securecapita.dto.*;
 import io.github.joaovitorleal.securecapita.dto.form.LoginFormDto;
+import io.github.joaovitorleal.securecapita.mapper.UserMapper;
 import io.github.joaovitorleal.securecapita.security.model.CustomUserDetails;
 import io.github.joaovitorleal.securecapita.security.provider.TokenProvider;
 import io.github.joaovitorleal.securecapita.service.UserService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -27,10 +27,11 @@ import static org.springframework.security.authentication.UsernamePasswordAuthen
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
-@Slf4j
+@Validated
 public class UserController {
 
     private final UserService userService;
+    private final UserMapper userMapper;
     private final AuthenticationManager authenticationManager;
     private final TokenProvider tokenProvider;
 
@@ -38,7 +39,7 @@ public class UserController {
     public ResponseEntity<ApiResponseDto> login(@RequestBody @Valid LoginFormDto loginForm) {
         Authentication authentication = authenticationManager.authenticate(unauthenticated(loginForm.email(), loginForm.password()));
         CustomUserDetails userPrincipal = (CustomUserDetails) authentication.getPrincipal();
-        UserResponseDto userResponseDto = userService.getUserDtoByEmail(loginForm.email());
+        UserResponseDto userResponseDto = userMapper.toResponseDto(userPrincipal.getUser());
         return userResponseDto.usingMfa()
                 ? this.sendVerificationCode(userResponseDto)
                 : this.sendLoginSuccessResponse(userResponseDto, userPrincipal);
@@ -60,15 +61,16 @@ public class UserController {
     }
 
     @PostMapping("/verify/code")
-    public ResponseEntity<ApiResponseDto> verifyCode(@RequestBody @Valid MfaVerificationRequestDto mfaVerificationRequestDto) {
-        UserResponseDto userResponseDto = userService.verifyMfaCode(mfaVerificationRequestDto.email(), mfaVerificationRequestDto.code());
+    public ResponseEntity<ApiResponseDto> verifyMfaCode(@RequestBody @Valid MfaVerificationRequestDto mfaVerificationRequestDto) {
+        User user = userService.verifyMfaCode(mfaVerificationRequestDto.email(), mfaVerificationRequestDto.code());
+        CustomUserDetails userPrincipal = new CustomUserDetails(user);
         return ResponseEntity.ok(
                 ApiResponseDto.builder()
                         .timestamp(LocalDateTime.now().toString())
                         .data(Map.of(
-                                "user", userResponseDto,
-                                "access_token", tokenProvider.createAccessToken(this.getUserPrincipal(userResponseDto)),
-                                "refresh_token", tokenProvider.createRefreshToken(this.getUserPrincipal(userResponseDto))
+                                "user", userMapper.toResponseDto(user),
+                                "access_token", tokenProvider.createAccessToken(userPrincipal),
+                                "refresh_token", tokenProvider.createRefreshToken(userPrincipal)
                         ))
                         .message("Login successful")
                         .status(HttpStatus.OK)
@@ -90,6 +92,53 @@ public class UserController {
                         .build()
         );
     }
+
+    // INÍCIO - Sistema de redefinição de senha para usuário não autenticado.
+
+    @PostMapping("/password-resets/{email}")
+    public ResponseEntity<ApiResponseDto> resetPassword(@PathVariable @Email(message = "Invalid email.") String email) {
+        userService.resetPassword(email);
+        return ResponseEntity.accepted().body(
+                ApiResponseDto.builder()
+                        .timestamp(LocalDateTime.now().toString())
+                        .message("Email sent. Please check your email to reset your password.")
+                        .status(HttpStatus.ACCEPTED)
+                        .statusCode(HttpStatus.ACCEPTED.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/verify/password/{token}")
+    public ResponseEntity<ApiResponseDto> verifyResetPasswordToken(@PathVariable @NotBlank(message = "The token is required.") String token) {
+        UserResponseDto userResponseDto = userService.verifyResetPasswordToken(token);
+        return ResponseEntity.ok(
+                ApiResponseDto.builder()
+                        .timestamp(LocalDateTime.now().toString())
+                        .data(Map.of("user", userResponseDto))
+                        .message("Please enter a new password.")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    @PostMapping("/password-resets/{token}")
+    public ResponseEntity<ApiResponseDto> resetPasswordByKey(
+            @PathVariable @NotBlank(message = "The token is required.") String token,
+            @RequestBody @Valid ResetPasswordRequestDto resetPasswordRequestDto
+    ) {
+        userService.resetPassword(token, resetPasswordRequestDto.newPassword(), resetPasswordRequestDto.confirmPassword());
+        return ResponseEntity.ok(
+                ApiResponseDto.builder()
+                        .timestamp(LocalDateTime.now().toString())
+                        .message("Password reset successfully.")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    // FIM - Sistema de redefinição de senha para usuário não autenticado.
 
     private ResponseEntity<ApiResponseDto> sendLoginSuccessResponse(UserResponseDto userResponseDto, CustomUserDetails userPrincipal) {
         return ResponseEntity.ok(
@@ -119,10 +168,5 @@ public class UserController {
                             .statusCode(HttpStatus.ACCEPTED.value())
                             .build()
                 );
-    }
-
-    private CustomUserDetails getUserPrincipal(UserResponseDto userResponseDto) {
-        User user = userService.getUserByEmail(userResponseDto.email());
-        return new CustomUserDetails(user, user.getRole().getPermission());
     }
 }
