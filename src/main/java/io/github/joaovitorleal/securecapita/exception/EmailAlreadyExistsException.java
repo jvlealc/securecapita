95abf01b72e8ef7a688fbb0ade0c166e90c5910f
joaoVitorLeal <joaoleal98@outlook.com>
2026-02-06 22:36:48 -0300

    feat: improve exception message formatting

--- src/main/java/io/github/joaovitorleal/securecapita/exception/EmailAlreadyExistsException.java
+++ src/main/java/io/github/joaovitorleal/securecapita/exception/EmailAlreadyExistsException.java
@@ -2,7 +2,7 @@ package io.github.joaovitorleal.securecapita.exception;
 
 public class EmailAlreadyExistsException extends ApiException {
 
-    public EmailAlreadyExistsException(String message) {
-        super(message);
+    public EmailAlreadyExistsException(String email) {
+        super("Email '" + email + "' already exists. Please use a different email and try again.");
     }
 }
